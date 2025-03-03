8375438cf94727d84c84d3071921023f12144af2
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2025-03-03 10:20:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import mysql.connector
 mydb = mysql.connector.connect(host="10.0.41.8", user= "nutzer14",password="Eftg8xdx",database="Datenbank14")
 mycursor = mydb.cursor()
 
+anzahlFragen = input("Wie viele Fragen willst du bekommen?")
+
+for i in range(1, anzahlFragen):
+  fragenNr = randi()
+  match 
 sql = "SELECT Name FROM land"
 mycursor.execute(sql)
 result = mycursor.fetchall()
